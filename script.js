83455314c9e02a5e245a112c5da85ced00e9c01a
Brayan Henrique Lima <brayanhenriquebsb@gmail.com>
2023-10-03 22:49:14 -0300

    Primeiro Commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function allowDrop(event) {
+  event.preventDefault();
+}
+
+function drag(event) {
+  event.dataTransfer.setData("text", event.target.id)
+}
+
+function drop(event) {
+  event.preventDefault();
+  let data =  event.dataTransfer.getData("text");
+  event.target.appendChild(document.getElementById(data));
+}
+
+
+function resetGame() { 
+  location.reload();
+}
